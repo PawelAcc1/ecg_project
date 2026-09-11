06e49e61bcd3c84c2014f7c0773b138fb04ba129
PawelAcc1 <pawelbukowiec66@gmail.com>
2026-09-12 01:54:36 +0200

    redundat mouse pos sync buffers cleared

--- projekt_basys3/rtl/GUI/draw_mouse.sv
+++ projekt_basys3/rtl/GUI/draw_mouse.sv
@@ -9,9 +9,6 @@ module draw_mouse (
 
 import vga_pkg::*;
 
-logic [11:0] x_sync1, x_sync2;
-logic [11:0] y_sync1, y_sync2;
-
 always_ff @(posedge clk or negedge rst_n) begin
     if (!rst_n) begin
         vga_out.vcount <= '0;
@@ -20,11 +17,6 @@ always_ff @(posedge clk or negedge rst_n) begin
         vga_out.hcount <= '0;
         vga_out.hblnk <= '0;
         vga_out.vblnk <= '0;
-
-        x_sync1 <= '0;
-        x_sync2 <= '0;
-        y_sync1 <= '0;
-        y_sync2 <= '0;
     end
     else begin
         vga_out.vcount <= vga_in.vcount;
@@ -33,17 +25,13 @@ always_ff @(posedge clk or negedge rst_n) begin
         vga_out.hcount <= vga_in.hcount;
         vga_out.hblnk <= vga_in.hblnk;
         vga_out.vblnk <= vga_in.vblnk;
-        x_sync1 <= x_start;
-        y_sync1 <= y_start;
-        x_sync2 <= x_sync1;
-        y_sync2 <= y_sync1;
     end
 end
 
 MouseDisplay u_MouseDisplay (
     .pixel_clk(clk),
-    .xpos(x_sync2),
-    .ypos(y_sync2),
+    .xpos(x_start),
+    .ypos(y_start),
     .hcount(vga_in.hcount[10:0]),
     .vcount(vga_in.vcount[10:0]),
     .blank(vga_in.hblnk | vga_in.vblnk),
